cfea6e9b565ef40c5797f9765b2d99ca7cbaadbd
Konstantin <konstantin@atomauto.ru>
2022-12-28 11:06:30 +0400

    need help

--- task_1/model/cart.go
+++ task_1/model/cart.go
@@ -27,7 +27,7 @@ func NewCart(User *User) *Cart {
 	return &Cart{uuid.New(), User, nil, nil, nil, nil, 0, 0, 0, time.Now()}
 }
 
-func (c *Cart) addProductToCart(p *Product, price float32, quantity uint16) {
+func (c *Cart) AddProductToCart(p *Product, price float32, quantity uint16) {
 	if p.minimumPrice >= price {
 		price = p.minimumPrice
 		fmt.Printf("Sorry, the minimal price for product is %f", p.minimumPrice)
@@ -50,7 +50,7 @@ func (c *Cart) addProductToCart(p *Product, price float32, quantity uint16) {
 	c.totalSum += float64(price)*float64(quantity)
 }
 
-func (c *Cart) changeQuantity(p *Product, quantity uint16) {
+func (c *Cart) ChangeQuantity(p *Product, quantity uint16) {
 	if quantity < p.minimumQuantity {
 		quantity = p.minimumQuantity
 		fmt.Printf("Sorry, the minimal amount to purchase is %v", quantity)
@@ -67,13 +67,7 @@ func (c *Cart) changeQuantity(p *Product, quantity uint16) {
 	c.totalSum += float64(c.prices[?])*float64(quantity)
 }
 
-func remove(s []int, i int) []int {
-    s[i] = s[len(s)-1]
-    return s[:len(s)-1]
-}
-
-
-func (c *Cart) deleteProductFromCart(p *Product) {
+func (c *Cart) DeleteProductFromCart(p *Product) {
 	c.Products[?] = c.Products[len(c.Products)-1]
 	c.Products = c.Products[:len(c.Products)-1]
 	c.quantity[?] = c.quantity[len(c.quantity)-1]
@@ -85,4 +79,8 @@ func (c *Cart) deleteProductFromCart(p *Product) {
 	c.totalVolume -= p.volume*float32(p.quantity[?])
 	c.totalSum -= float64(prices[?])*float64(p.quantity[?])
 	//Maybe, we should have created new structure cartItem, because in current realisation we cannot identify cart item properly 
+}
+
+func (c *Cart) Delete() {
+	c = nil
 }
