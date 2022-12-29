a8f15a1cf7f68c0f4538dcbcbab372b37b29ffe2
Konstantin <konstantin@atomauto.ru>
2022-12-29 13:47:04 +0400

    Initial (only readme)

--- task_2/cmd/main.go
+++ task_2/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"task_2/model"
 	"task_2/service"
+	"time"
 )
 
 var storage *service.MemoryStorage
@@ -27,6 +28,20 @@ func UserRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func UserLoginRequest(w http.ResponseWriter, r *http.Request) {
+	expirationTime := time.Now().Add(5 * time.Minute)
+	// Create the JWT claims, which includes the username and expiry time
+	claims := &Claims{
+		Username: creds.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
 	fmt.Fprintf(w, "Login user")
 }
 func main() {
